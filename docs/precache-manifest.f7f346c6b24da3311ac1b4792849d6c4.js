066f631005791719d7d422d92b600755730b7532
Leonardo Cortes <leodistrital@hotmail.com>
2020-08-02 00:58:10 -0500

    docs agredado

---
+++ docs/precache-manifest.f7f346c6b24da3311ac1b4792849d6c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a824fb3bcfdf63da884044bdb3bd27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7083742f9c328995c8f6",
+    "url": "/static/css/main.96f08ab7.chunk.css"
+  },
+  {
+    "revision": "48fe85f264d9d99d1710",
+    "url": "/static/js/2.8ea88baf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ea88baf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7083742f9c328995c8f6",
+    "url": "/static/js/main.b31882e7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
